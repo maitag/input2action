67d0aa9f3ba516a23809d8db811a087331bec0aa
maitag <info@maitag.de>
2024-08-22 04:50:35 +0200

    fixing ActionMap to cast map-literals

--- src/input2action/ActionConfig.hx
+++ src/input2action/ActionConfig.hx
@@ -7,11 +7,12 @@ import lime.ui.GamepadButton;
 @:forward
 abstract ActionConfig(Array<ActionConfigItem>) from Array<ActionConfigItem> to Array<ActionConfigItem>
 {
-/*	public inline function new()
-	{
+	/*
+	public inline function new() {
 		this = new Array<ActionConfigItem>();
 	}
-*/	
+	*/
+
 	public static function fromJson(jsonString:String, debugFilename:String = ""):ActionConfig {
 		return JsonConfig.fromString(jsonString, debugFilename).toActionConfig();
 	}
@@ -42,4 +43,3 @@ class ActionConfigItem {
 	public var gamepad :NestedArray<GamepadButton> = null;
 	public var joystick:NestedArray<Int> = null;	
 }
-
--- src/input2action/ActionMap.hx
+++ src/input2action/ActionMap.hx
@@ -2,20 +2,29 @@ package input2action;
 
 import haxe.ds.StringMap;
 import haxe.ds.Map;
-import input2action.ActionFunction;
 
 // typedef ActionMap = haxe.ds.StringMap<ActionMapItem>;
 
 @:forward
 abstract ActionMap(StringMap<ActionMapItem>) from StringMap<ActionMapItem> to StringMap<ActionMapItem>
 {
-/*	public inline function new()
-	{
+	/*
+	public inline function new() {
 		this = new StringMap<ActionMapItem>();
 	}
-*/
+	*/
+
+	/*
+	// this did not work if ActionMapItem is a @:structInit
 	@:from
 	public static inline function fromStringMap(map:Map<String, ActionMapItem>):ActionMap {
+		return cast map;
+	}
+	*/
+
+	// helper to cast also from Map<String, ...> while give values by map-literal-syntax 
+	@:from
+	public static inline function fromStringMapAny(map:Map<String, ActionMapItemAny>):ActionMap {
 		return cast map;
 	}
 
@@ -27,6 +36,7 @@ abstract ActionMap(StringMap<ActionMapItem>) from StringMap<ActionMapItem> to St
 
 }
 
+/*
 typedef ActionMapItem = {
 	action:ActionFunction,
 	?description:String,
@@ -38,3 +48,32 @@ typedef ActionMapItem = {
 	?repeatRate:Null<Int>
 	#end
 }
+*/
+
+@:structInit
+class ActionMapItem {
+	public var action:ActionFunction;
+	public var description:String = null;
+	public var up:Null<Bool> = null;
+	public var each:Null<Bool> = null;
+	#if !input2action_noRepeat
+	public var repeatKeyboardDefault:Null<Bool> = null;
+	public var repeatDelay:Null<Int> = null;
+	public var repeatRate:Null<Int> = null;
+	#end
+}
+
+
+// helper to cast also from Map<String, ...> while give values by map-literal-syntax 
+private typedef ActionMapItemAny = {
+	action:Any->Any->Void,
+	?description:String,
+	?up:Null<Bool>,
+	?each:Null<Bool>,
+	#if !input2action_noRepeat
+	?repeatKeyboardDefault:Null<Bool>,
+	?repeatDelay:Null<Int>,
+	?repeatRate:Null<Int>
+	#end
+}
+
